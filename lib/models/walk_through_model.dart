483d139c82701f0043b48425fb23f074d329c82f
ZakariaAshraf <145775945+ZakariaAshraf@users.noreply.github.com>
2026-01-01 04:14:11 +0200

    add walk through new screen to choose application language before your first start

--- lib/models/walk_through_model.dart
+++ lib/models/walk_through_model.dart
@@ -7,3 +7,16 @@ class WalkThroughModel {
 
   WalkThroughModel({this.image, this.title,this.title1,this.title2});
 }
+class IntroLanguageModel {
+  String name;
+  String code;
+  String flagImage;
+  String countryCode;
+
+  IntroLanguageModel({
+    required this.name,
+    required this.code,
+    required this.flagImage,
+    required this.countryCode,
+  });
+}
--- lib/screens/walk_through_screen.dart
+++ lib/screens/walk_through_screen.dart
@@ -5,16 +5,18 @@ import '../../extensions/app_button.dart';
 import '../../extensions/extension_util/context_extensions.dart';
 import '../../extensions/extension_util/int_extensions.dart';
 import '../../extensions/extension_util/widget_extensions.dart';
+import '../extensions/colors.dart';
 import '../extensions/common.dart';
+import '../extensions/decorations.dart';
 import '../extensions/shared_pref.dart';
 import '../extensions/text_styles.dart';
+import '../languageConfiguration/LanguageDataConstant.dart';
 import '../main.dart';
 import '../models/walk_through_model.dart';
 
 import '../utils/colors.dart';
 import '../utils/constants.dart';
 import '../utils/images.dart';
-
 class WalkThroughScreen extends StatefulWidget {
   const WalkThroughScreen({super.key});
 
@@ -26,6 +28,7 @@ class _WalkThroughScreenState extends State<WalkThroughScreen> {
   PageController mPageController = PageController();
 
   List<WalkThroughModel> mWalkList = [];
+  List<IntroLanguageModel> mLanguages = [];
   int mCurrentIndex = 0;
 
   @override
@@ -35,6 +38,46 @@ class _WalkThroughScreenState extends State<WalkThroughScreen> {
   }
 
   init() async {
+    mLanguages.add(IntroLanguageModel(
+        name: "العربية",
+        code: "ar",
+        countryCode: "ar-EG",
+        flagImage: "assets/eg.png"));
+    mLanguages.add(IntroLanguageModel(
+        name: "English",
+        code: "en",
+        countryCode: "en-US",
+        flagImage: "assets/us.png"));
+    mLanguages.add(IntroLanguageModel(
+        name: "Русский",
+        code: "ru",
+        countryCode: "ru-RU",
+        flagImage: "assets/ru.png"));
+  }
+
+  @override
+  void setState(fn) {
+    if (mounted) super.setState(fn);
+  }
+
+  @override
+  void dispose() {
+    mPageController.dispose();
+    super.dispose();
+  }
+  void _selectLanguage(IntroLanguageModel langData) async {
+    await setValue(SELECTED_LANGUAGE_CODE, langData.code);
+    await setValue(SELECTED_LANGUAGE_COUNTRY_CODE, langData.countryCode);
+    await setValue(IS_SELECTED_LANGUAGE_CHANGE, true);
+
+    appStore.setLanguage(langData.code, context: context);
+
+    mPageController.nextPage(
+        duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+  }
+  @override
+  Widget build(BuildContext context) {
+    mWalkList.clear();
     mWalkList.add(WalkThroughModel(
         image: ic_walk1,
         title: language.WALK1_TITLE,
@@ -50,21 +93,7 @@ class _WalkThroughScreenState extends State<WalkThroughScreen> {
         title: language.WALK3_TITLE,
         title1: language.WALK3_TITLE3,
         title2: language.WALK3_TITLE_3));
-  }
-
-  @override
-  void setState(fn) {
-    if (mounted) super.setState(fn);
-  }
-
-  @override
-  void dispose() {
-    mPageController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+    int totalPages = mWalkList.length + 1;
     return AnnotatedRegion(
       value: SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
@@ -76,90 +105,119 @@ class _WalkThroughScreenState extends State<WalkThroughScreen> {
       child: Scaffold(
           body: Stack(
         children: [
-          PageView(
+          PageView.builder(
             controller: mPageController,
-            children: mWalkList.map((e) {
+            itemCount: totalPages,
+            onPageChanged: (i) {
+              setState(() {
+                mCurrentIndex = i;
+              });
+            },
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(ic_logo, height: 100, width: 100).paddingBottom(30),
+
+                    Text("Choose Your Language", style: boldTextStyle(size: 24)),
+                    Text("اختر لغتك المفضلة", style: secondaryTextStyle(size: 16)),
+                    30.height,
+                    ...mLanguages.map((lang) {
+                      return Container(
+                        margin: EdgeInsets.symmetric(horizontal: 40, vertical: 8),
+                        clipBehavior: Clip.antiAlias,
+                        decoration: boxDecorationWithRoundedCorners(
+                          backgroundColor:
+                          appStore.isDarkModeOn ? cardDarkColor : Colors.grey.shade100,
+                          border: Border.all(color: primaryColor.withOpacity(0.2)),
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: () => _selectLanguage(lang),
+                            child: Padding(
+                              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(lang.flagImage, width: 32, height: 32),
+                                  16.width,
+                                  Text(lang.name, style: boldTextStyle()),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ],
+                ).center();
+              }
+              int walkIndex = index - 1;
               return Column(
                 children: [
-                  Image.asset(mWalkList[mCurrentIndex].image!,
+                  Image.asset(mWalkList[walkIndex].image!,
                       height: context.height() * 0.50, fit: BoxFit.fill),
                 ],
               ).paddingTop(context.statusBarHeight + 30);
-            }).toList(),
-            onPageChanged: (i) {
-              mCurrentIndex = i;
-              setState(() {});
             },
           ),
-          // Positioned(
-          //   top: context.statusBarHeight,
-          //   right: 4,
-          //   child: TextButton(
-          //       style: ButtonStyle(
-          //           overlayColor:
-          //               WidgetStateProperty.all(Colors.transparent)),
-          //       onPressed: () {
-          //         setValue(IS_FIRST_TIME, true);
-          //         JoinUsScreen().launch(context, isNewTask: true);
-          //         // LoginScreen().launch(context, isNewTask: false);
-          //       },
-          //       child: Text(language.skip,
-          //           style: boldTextStyle(color: primaryColor))),
-          // ).visible(mCurrentIndex != 2),
-          Positioned(
-            right: 24,
-            left: 24,
-            bottom: context.statusBarHeight + 70,
-            child: Column(
-              children: [
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: mWalkList[mCurrentIndex]
-                              .title1
-                              .toString()
-                              .toUpperCase(),
-                          style: boldTextStyle(size: 20)),
-                      TextSpan(
-                          text: " ${mWalkList[mCurrentIndex]
-                                  .title2
-                                  .toString()
-                                  .toUpperCase()}",
-                          style: boldTextStyle(color: primaryColor, size: 20)),
-                    ],
+          if (mCurrentIndex > 0)
+            Positioned(
+              right: 24,
+              left: 24,
+              bottom: context.statusBarHeight + 70,
+              child: Column(
+                children: [
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: mWalkList[mCurrentIndex - 1]
+                                .title1
+                                .toString()
+                                .toUpperCase(),
+                            style: boldTextStyle(size: 20)),
+                        TextSpan(
+                            text: " ${mWalkList[mCurrentIndex - 1]
+                                .title2
+                                .toString()
+                                .toUpperCase()}",
+                            style: boldTextStyle(color: primaryColor, size: 20)),
+                      ],
+                    ),
                   ),
-                ),
-                16.height,
-                Text(mWalkList[mCurrentIndex].title.toString(),
-                    style: secondaryTextStyle(size: 16),
-                    textAlign: TextAlign.center),
-                16.height,
-                dotIndicator(mWalkList, mCurrentIndex),
-                40.height,
-                AppButton(
-                  text:
-                      mCurrentIndex == 2 ? language.getStarted : language.next,
-                  width: context.width(),
-                  color: primaryColor,
-                  onTap: () {
-                    if (mCurrentIndex.toInt() >= 2) {
-                      setValue(IS_FIRST_TIME, true);
-                      JoinUsScreen().launch(context, isNewTask: true);
-                      // LoginScreen().launch(context, isNewTask: false);
-                    } else {
-                      mPageController.nextPage(
-                          duration: Duration(seconds: 1),
-                          curve: Curves.linearToEaseOut);
-                    }
-                  },
-                ),
-              ],
+                  16.height,
+                  Text(mWalkList[mCurrentIndex - 1].title.toString(),
+                      style: secondaryTextStyle(size: 16),
+                      textAlign: TextAlign.center),
+                  16.height,
+                  dotIndicator(mWalkList, mCurrentIndex - 1),
+
+                  40.height,
+                  AppButton(
+                    text: (mCurrentIndex - 1) == 2 ? language.getStarted : language.next,
+                    width: context.width(),
+                    color: primaryColor,
+                    onTap: () {
+                      if ((mCurrentIndex - 1) >= 2) {
+                        setValue(IS_FIRST_TIME, true);
+                        JoinUsScreen().launch(context, isNewTask: true);
+                      } else {
+                        mPageController.nextPage(
+                            duration: Duration(seconds: 1),
+                            curve: Curves.linearToEaseOut);
+                      }
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
         ],
-      )),
+          ),
+      ),
     );
   }
 }
